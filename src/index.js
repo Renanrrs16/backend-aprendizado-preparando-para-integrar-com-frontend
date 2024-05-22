9453e46dfae3ee44f05ebddcab71190a2180543b
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-05-22 16:19:28 +0000

    implementação de erros inesperados

--- src/index.js
+++ src/index.js
@@ -28,6 +28,12 @@ async function main() {
   //Router
   app.use('/personagem', personagemRouter)
 
+  //erro Handling
+  app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Algo deu errado!' });
+  });
+
   app.listen(3000, function () {
     console.log('Aplicação rodando em http://localhost:3000')
   })
