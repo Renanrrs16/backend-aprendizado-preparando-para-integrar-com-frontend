70a95b607de0115beda2214be410b0efccb91d90
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-05-22 18:49:56 +0000

    Campos `imagem` e `evoluiPara` adicionados

--- src/personagem/personagem.entity.js
+++ src/personagem/personagem.entity.js
@@ -1,11 +1,20 @@
 const Joi = require('joi');
 
 const personagem = Joi.object({
-  nome: Joi
-    .string()
-    .min(3)
+  nome: Joi.string()
+    .min(1)
     .max(30)
     .required(),
+
+  imagem: Joi.string()
+    .uri()
+    .required(),
+
+  evoluiPara: Joi.string()
+    .min(1)
+    .max(30)
+    .optional()
 });
+
 
 module.exports = personagem
